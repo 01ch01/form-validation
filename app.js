632e0044f6c02b029a1f0598e21ee769b4835464
01ch01 <00claudio.henrique@gmail.com>
2021-02-09 14:44:57 -0300

    upload files

---
+++ app.js
@@ -0,0 +1,74 @@
+const passwordEl = document.querySelector('#password');
+const buttonEl = document.querySelector('#btn');
+
+const lowerUpper = document.querySelector('#lower-upper');
+const number = document.querySelector('#numbers');
+const specialCharacters = document.querySelector('#special-characters');
+const digits = document.querySelector('#digits');
+
+passwordEl.addEventListener('keyup', event => {
+  const { value } = passwordEl;
+
+  const isLowerUpperValidate = validateLowerUpperCase(value);
+  toggleRequirements(lowerUpper, isLowerUpperValidate);
+
+  const isNumberValidate = validateNumbers(value);
+  toggleRequirements(number, isNumberValidate);
+
+  const isSpecialCharactersValidate = validateSpecialCharacters(value);
+  toggleRequirements(specialCharacters, isSpecialCharactersValidate);
+
+  const isDigitsValidate = validateDigits(value);
+  toggleRequirements(digits, isDigitsValidate);
+
+  const isValidate =
+    isLowerUpperValidate &&
+    isNumberValidate &&
+    isSpecialCharactersValidate &&
+    isDigitsValidate;
+
+  toggleButton(isValidate);
+});
+
+function validateLowerUpperCase(value) {
+  const upper = RegExp('[A-Z]');
+  const lower = RegExp('[a-z]');
+
+  return value.match(lower) && value.match(upper);
+}
+
+function validateNumbers(value) {
+  const regex = /\d/g;
+  return value.match(regex);
+}
+
+function validateSpecialCharacters(value) {
+  const regex = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  return value.match(regex);
+}
+
+function validateDigits(value) {
+  return value.length >= 8;
+}
+
+function toggleRequirements(item, isActive) {
+  const icon = item.getElementsByTagName('i')[0];
+
+  if (isActive) {
+    item.classList.add('active');
+    icon.classList.remove('fa-times-circle');
+    icon.classList.add('fa-check');
+  } else {
+    item.classList.remove('active');
+    icon.classList.add('fa-times-circle');
+    icon.classList.remove('fa-check');
+  }
+}
+
+function toggleButton(isFormValidated) {
+  if (isFormValidated) {
+    buttonEl.removeAttribute('disabled');
+  } else {
+    buttonEl.setAttribute('disabled', '');
+  }
+}
